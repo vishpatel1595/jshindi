50bcafa9592b992ffc892d6e02953fa335d890a2
Vishal Patel <31820098+vishpatel1595@users.noreply.github.com>
2023-08-23 07:24:58 +0000

    conversion of  datatype

---
+++ conversionoperation.js
@@ -0,0 +1,22 @@
+let score = "viki"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber) ;
+console.log(valueInNumber);
+
+
+// number "33"  => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
