6e9a07a09c0fd199493c569172509b83c68289bd
Vishal Patel <31820098+vishpatel1595@users.noreply.github.com>
2023-08-23 06:27:14 +0000

    new learning datatype

---
+++ basic/datatypesecma.js
@@ -0,0 +1,23 @@
+"use strict"; // trick all js code as newer version
+
+// window.alert (3 + 3)  // we are using nodejs not browser
+console.log(3 + 3);
+console.log("Viki");
+
+let name = "viki"
+let age = "28"
+let isLoggedIn = false 
+// let state; undefined
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value  {empty}
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof "viki") ;
+
--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,18 @@
 console.log('hello');
+const accountId = 17
+let accountEmail = "jarvis@google.com"
+var accountPassword = "12345"
+
+accountCity = "Vadodara"
+
+// accountId = 2
+accountEmail = "viki@gmail.com"
+accountPassword = "17171"
+accountCity = "Valsad"
+
+/*
+preffer not to use var in js due to scope is not used in it  
+*/
+
+console.log(accountId);
+console.table([accountCity , accountEmail , accountPassword  ]);
